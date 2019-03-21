ea093cfff505231927d09275116d0c2a9cd6f511
I-Lin <ian.lin20@ycdsbk12.ca>
2019-03-21 09:48:41 -0400

    Start Coin.py

--- Coin.py
+++ Coin.py
@@ -1,7 +1,22 @@
+import random
+
 # Define your Coin class here
+class Coin(object):
+
+    def __init__(self):
+        Coin.face = random.randint(1, 2)
+
+    def get_face(self):
+        if self.face == 1:
+            return "heads"
+        else:
+            return "tails"
+
+    def flip(self):
+        Coin.face = random.randint(1, 2)
 
 
 if __name__ == '__main__':
-  # put your code that utilizes your Coin class here
-  pass
+    # put your code that utilizes your Coin class here
+    pass
   
