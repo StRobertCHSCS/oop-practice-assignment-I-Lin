2314c730ccab5670caaeb77a02c9b5dbeeb6fb95
I-Lin <ian.lin20@ycdsbk12.ca>
2019-03-21 20:47:06 -0400

    Do practice questions

--- Coin.py
+++ Coin.py
@@ -1,5 +1,6 @@
 import random
 
+
 # Define your Coin class here
 class Coin(object):
 
@@ -16,7 +17,23 @@ class Coin(object):
         Coin.face = random.randint(1, 2)
 
 
-if __name__ == '__main__':
-    # put your code that utilizes your Coin class here
-    pass
-  
+def main():
+    Coin1 = Coin()
+    num_heads = 0
+    num_tails = 0
+    for i in range(1000):
+        if Coin1.get_face() == "heads":
+            num_heads += 1
+        else:
+            num_tails += 1
+        Coin1.flip()
+
+    print("The number of head flips is", num_heads)
+    print("The number of tail flips is", num_tails)
+
+
+main()
+# if __name__ == '__main__':
+#     # put your code that utilizes your Coin class here
+#     pass
+#
--- Fraction.py
+++ Fraction.py
@@ -1,6 +1,56 @@
 # Define your Fraction class here
+class Fraction():
+
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def get_numerator(self):
+        return self.numerator
+
+    def get_denominator(self):
+        return self.denominator
+
+    def set_numerator(self, new_numerator):
+        self.numerator = new_numerator
+
+    def set_denominator(self, new_denominator):
+        if new_denominator == 0:
+            print("Denominator cannot be 0")
+        else:
+            self.denominator = new_denominator
+
+    def __str__(self):
+        return self.numerator + '/' + self.denominator
+
+    def add(self, other_fraction):
+        numerator_sum = str(int(self.numerator)*int(other_fraction.denominator) + int(other_fraction.numerator)*int(self.denominator))
+        denominator_sum = str(int(self.denominator)*int(other_fraction.denominator) + int(other_fraction.denominator)*int(self.denominator))
+        return numerator_sum + "/" + denominator_sum
+
+    def subtract(self, other_fraction):
+        numerator_diff = str(int(self.numerator)*int(other_fraction.denominator) - int(other_fraction.numerator)*int(self.denominator))
+        denominator = str(int(self.denominator)*int(other_fraction.denominator))
+        return numerator_diff + "/" + denominator
+
+    def multiply(self, other_fraction):
+        numerator_product = str(int(self.numerator) * int(other_fraction.numerator))
+        denominator_product = str(int(self.denominator) * int(other_fraction.denominator))
+        return numerator_product + "/" + denominator_product
+
+    # def __reduce__(self):
+        # Fraction(numerator, denominator)
+
+def main():
+    fraction1 = Fraction("2", "3")
+    fraction2 = Fraction("3", "4")
+    # print(fraction1)
+    print(fraction1.add(fraction2))
+    print(fraction1.subtract(fraction2))
+    print(fraction1.multiply(fraction2))
 
 
-if __name__ == '__main__':
-  # put your code that utilizes your Fraction class here
-  pass
+main()
+# if __name__ == '__main__':
+#     # put your code that utilizes your Fraction class here
+#     pass
